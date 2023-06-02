6b4f744de7d846f5df7b2345f354b6a33cb82d91
lucianoAgustin10 <134083011+lucianoAgustin10@users.noreply.github.com>
2023-06-02 14:15:06 -0300

    agrega instrucciones

--- ensalada.js
+++ ensalada.js
@@ -1,1 +1,8 @@
 console.log("esta ready la ensalada")
+console.log("
+            instrucciones 
+            1 lavar las manos
+            2 cortar la lechuga con la mano 
+            2 condimentar ensalada 
+            3 cortar todo chico 
+            ")
